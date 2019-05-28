b438b6d99be5bddf474a589005e431950155fb63
王亚东 <wyd@wangyadongdeMacBook-Pro.local>
2019-05-28 14:20:19 +0800

    git init git git remote add origin https://github.com/zifengmo/may-third.git

---
+++ may-third-controller/src/main/java/com/wyd/may/App.java
@@ -0,0 +1,13 @@
+package com.wyd.may;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
